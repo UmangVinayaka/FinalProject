cd04004f01a08615e7ec9c94cae218460491516a
rosawoo <137002166+rosawoo@users.noreply.github.com>
2023-11-28 09:14:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-import datetime
 import re
 
 from cs50 import SQL
@@ -12,16 +11,13 @@ from helpers import apology, login_required, lookup, usd
 # Configure application
 app = Flask(__name__)
 
-# Custom filter
-app.jinja_env.filters["usd"] = usd
-
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
+db = SQL("sqlite:///calendar.db")
 
 
 @app.after_request
@@ -31,13 +27,7 @@ def after_request(response):
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
     return response
-
-
-# transactions table
-db.execute(
-    "CREATE TABLE IF NOT EXISTS transactions (id INTEGER PRIMARY KEY, symbol TEXT, name TEXT, shares INTEGER, user_id INTEGER, price REAL, time TIMESTAMP, FOREIGN KEY(user_id) REFERENCES users(id))"
-)
-
+    
 
 @app.route("/")
 @login_required
@@ -63,71 +53,6 @@ def index():
         holdings=holdings[0]["cash"],
         grand_total=grand_total,
     )
-
-
-@app.route("/buy", methods=["GET", "POST"])
-@login_required
-def buy():
-    """Buy shares of stock"""
-
-    if request.method == "POST":
-        symbol = request.form.get("symbol")
-        looked_up_symbol = lookup(symbol)
-        if not symbol:
-            return apology("stock symbol must be provided")
-        if looked_up_symbol == None:
-            return apology("stock does not exist")
-
-        shares = request.form.get("shares")
-        if not shares:
-            return apology("shares must be provided")
-        try:
-            shares = int(shares)
-        except ValueError:
-            return apology("shares must be an integer")
-        if shares <= 0:
-            return apology("shares must positive")
-
-        price = looked_up_symbol["price"]
-        name = looked_up_symbol["name"]
-
-        user = db.execute(
-            "SELECT cash FROM users WHERE id = :user_id", user_id=session["user_id"]
-        )
-        cash_balance = user[0]["cash"]
-
-        shares_cost = price * shares
-        if shares_cost > cash_balance:
-            return apology("not enough in balance to buy")
-        else:
-            cash_balance -= shares_cost
-            db.execute(
-                "UPDATE users SET cash = :cash_balance WHERE id = :user_id",
-                cash_balance=cash_balance,
-                user_id=session["user_id"],
-            )
-            db.execute(
-                "INSERT INTO transactions (symbol, name, shares, user_id, price, time) VALUES(?, ?, ?, ?, ?, ?)",
-                symbol,
-                name,
-                shares,
-                session["user_id"],
-                price,
-                datetime.datetime.now(),
-            )
-            flash("Bought!")
-            return redirect("/")
-
-    else:
-        return render_template("buy.html")
-
-
-@app.route("/history")
-@login_required
-def history():
-    """Show history of transactions"""
-    history = db.execute("SELECT symbol, shares, price, time FROM transactions")
-    return render_template("history.html", history=history)
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -237,65 +162,3 @@ def register():
 
     else:
         return render_template("register.html")
-
-
-@app.route("/sell", methods=["GET", "POST"])
-@login_required
-def sell():
-    """Sell shares of stock"""
-    if request.method == "POST":
-        symbol = request.form.get("symbol")
-        if not symbol:
-            return apology("stock symbol invalid")
-
-        shares_owned = db.execute(
-            "SELECT SUM(shares) as total_shares FROM transactions WHERE symbol = :symbol",
-            symbol=symbol,
-        )
-        total_shares = shares_owned[0]["total_shares"]
-
-        shares = request.form.get("shares")
-        if not shares:
-            return apology("shares must be provided")
-        try:
-            shares = int(shares)
-        except ValueError:
-            return apology("shares must be an integer")
-        if shares <= 0:
-            return apology("shares must positive")
-        if shares > total_shares:
-            return apology("not enough shares owned")
-
-        looked_up_stock = lookup(symbol)
-        price = looked_up_stock["price"]
-        name = looked_up_stock["name"]
-
-        # update transactions table
-        db.execute(
-            "INSERT INTO transactions (symbol, name, shares, user_id, price, time) VALUES(?, ?, ?, ?, ?, ?)",
-            symbol,
-            name,
-            (-1 * shares),
-            session["user_id"],
-            price,
-            datetime.datetime.now(),
-        )
-
-        # add profit from stock sale to current cash holdings
-        additional_cash = price * shares
-        db.execute(
-            "UPDATE users SET cash = cash + :additional_cash WHERE id = :user_id",
-            additional_cash=additional_cash,
-            user_id=session["user_id"],
-        )
-
-        flash("Sold!")
-        return redirect("/")
-
-    else:
-        unique_stocks = db.execute(
-            "SELECT symbol FROM transactions WHERE user_id = :user_id GROUP BY symbol HAVING SUM(shares) > 0",
-            user_id=session["user_id"],
-        )
-
-        return render_template("sell.html", unique_stocks=unique_stocks)
